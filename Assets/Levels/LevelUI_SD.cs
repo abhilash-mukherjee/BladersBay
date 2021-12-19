fb7b104b9da0103fd138901b3908b062cde987e0
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2021-12-19 22:34:54 +0530

    Change in Design

--- Assets/Levels/LevelUI_SD.cs
+++ Assets/Levels/LevelUI_SD.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LevelUI_SD : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Image displayUI;
+    public Image displayUI,centerUI;
+    public TextMeshProUGUI displayText;
     public bool isFilled = false;
     public float timeElapsed = 0f;
 }
--- Assets/Levels/LevelsUI_SD.cs
+++ Assets/Levels/LevelsUI_SD.cs
@@ -24,9 +24,11 @@ public class LevelsUI_SD : MonoBehaviour
     {
         for (int i = 1; i < index; i++)
         {
-            levelNumberChild[i-1].GetComponent<LevelUI_SD>().displayUI.fillAmount = 1f;
+            levelNumberChild[i-1].displayUI.fillAmount = 1f;
+            levelNumberChild[i-1].centerUI.gameObject.SetActive(true);
+            // levelNumberChild[i-1].displayText.color = new Color()
             if (i != 1)
-                connectingChild[i - 2].GetComponent<LevelUI_SD>().displayUI.fillAmount = 1f;
+                connectingChild[i - 2].displayUI.fillAmount = 1f;
         }
         if (index == 1) StartCoroutine(FillStroke(0));
         else StartCoroutine(FillConnectingStroke(index - 2));
@@ -35,31 +37,33 @@ public class LevelsUI_SD : MonoBehaviour
 
     IEnumerator FillStroke(int index)
     {
-        float timeElapsed = levelNumberChild[index].GetComponent<LevelUI_SD>().timeElapsed;
-        while (levelNumberChild[index].GetComponent<LevelUI_SD>().displayUI.fillAmount < 1f)
+        float timeElapsed = levelNumberChild[index].timeElapsed;
+        while (levelNumberChild[index].displayUI.fillAmount < 1f)
         {
-            levelNumberChild[index].GetComponent<LevelUI_SD>().displayUI.fillAmount = levelNumberChild[index].GetComponent<LevelUI_SD>().timeElapsed;
-            levelNumberChild[index].GetComponent<LevelUI_SD>().timeElapsed += Time.deltaTime;
+            levelNumberChild[index].displayUI.fillAmount = levelNumberChild[index].timeElapsed;
+            levelNumberChild[index].timeElapsed += Time.deltaTime;
             yield return null;
         }
 
-        levelNumberChild[index].GetComponent<LevelUI_SD>().isFilled = true;
-        levelNumberChild[index].GetComponent<LevelUI_SD>().timeElapsed = 0f;
+        levelNumberChild[index].centerUI.gameObject.SetActive(true);
+
+        levelNumberChild[index].isFilled = true;
+        levelNumberChild[index].timeElapsed = 0f;
 
     }
 
     IEnumerator FillConnectingStroke(int index)
     {
         Debug.Log(index);
-        while (connectingChild[index].GetComponent<LevelUI_SD>().displayUI.fillAmount < 1f)
+        while (connectingChild[index].displayUI.fillAmount < 1f)
         {
-            connectingChild[index].GetComponent<LevelUI_SD>().displayUI.fillAmount = connectingChild[index].GetComponent<LevelUI_SD>().timeElapsed;
-            connectingChild[index].GetComponent<LevelUI_SD>().timeElapsed += Time.deltaTime;
+            connectingChild[index].displayUI.fillAmount = connectingChild[index].timeElapsed;
+            connectingChild[index].timeElapsed += Time.deltaTime;
             yield return null;
         }
 
-        connectingChild[index].GetComponent<LevelUI_SD>().isFilled = true;
-        connectingChild[index].GetComponent<LevelUI_SD>().timeElapsed = 0f;
+        connectingChild[index].isFilled = true;
+        connectingChild[index].timeElapsed = 0f;
         StartCoroutine(FillStroke(index + 1));
     }
 }
