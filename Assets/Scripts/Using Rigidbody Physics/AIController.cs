afb503bbd5b9bae7feab20bf48746966b027157a
Abhilash <abhilash301267@gmail.com>
2021-12-25 21:18:00 +0530

    Player data duplicated

---
+++ Assets/Scripts/Using Rigidbody Physics/AIController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AIController: InputController
+{
+    [SerializeField]
+    private KeyCode attackTrigger, defenceTrigger, staminaTrigger, balanceTrigger;
+    [SerializeField]
+    private GameEvent attackTriggerEvent, defenceTriggerEvent, staminaTriggerEvent, balanceTriggerEvent;
+    [SerializeField]
+    private StateController enemyStateController, playerStateController;
+    private AIState m_CurrentState;
+    private void Update()
+    {
+        m_CurrentState.UpdateState(enemyStateController, playerStateController,this);
+    }
+}
