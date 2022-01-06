34689cd6618ee9c50b850d5b5ee70bba9d9e7414
Subhojit-Dey1234 <subhojit2000dey@gmail.com>
2022-01-06 18:04:25 +0530

    Before changing Ham

--- Assets/Levels/ClickPlay_SD.cs
+++ Assets/Levels/ClickPlay_SD.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickPlay_SD : MonoBehaviour
 {
@@ -8,12 +9,17 @@ public class ClickPlay_SD : MonoBehaviour
     [SerializeField]
     private bool isPlayClick = true;
     [SerializeField]
-    private GameObject infoButton;
+    private GameObject infoButton,profile;
     [SerializeField]
     private LevelButtonFillAnimationManager LevelButtonFillAnimationManager;
+
+    [SerializeField]
+    private Image background;
     public void StartUIPopUpPopDown(bool isReversed){
         isPlayClick = !isPlayClick;
-        infoButton.SetActive(isPlayClick);
+        // infoButton.SetActive(isPlayClick);
+        // profile.SetActive(isPlayClick);
+        background.enabled = isPlayClick;
         LevelButtonFillAnimationManager.StartLevelFillAnimation();
         foreach(var levelsUINew_SD in levelsUINew_SDs){
             levelsUINew_SD.AnimateUIStart(isReversed);
